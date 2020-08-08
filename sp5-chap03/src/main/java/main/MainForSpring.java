7b634f15836ed4651203bac75019e2525d4e41c2
Taewoo Lee <lteawoo@naver.com>
2020-08-08 09:59:13 +0900

    setter di

--- sp5-chap03/src/main/java/main/MainForSpring.java
+++ sp5-chap03/src/main/java/main/MainForSpring.java
@@ -3,7 +3,15 @@ package main;
 import config.AppCtx;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import spring.*;
+import spring.ChangePasswordService;
+import spring.DuplicateMemberException;
+import spring.MemberInfoPrinter;
+import spring.MemberListPrinter;
+import spring.MemberNotFoundException;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
+import spring.VersionPrinter;
+import spring.WrongIdPasswordException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,11 +38,18 @@ public class MainForSpring {
         processChangeCommand(command.split(" "));
       } else if (command.startsWith("list")) {
         processListCommand();
-      } else if (command.startsWith("info ")) {
+      } else if (command.startsWith("info")) {
         processInfoCommand(command.split(" "));
+      } else if (command.startsWith("version")) {
+        processVersionCommand();
       }
     }
     printHelp();
+  }
+
+  private static void processVersionCommand() {
+    VersionPrinter versionPrinter = ctx.getBean("versionPrinter", VersionPrinter.class);
+    versionPrinter.print();
   }
   
   private static void processListCommand() {
